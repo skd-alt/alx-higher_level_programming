9265546e0f035cbdfe1add878d7805e2a41a84c1
skd-alt <sipho@skanalytics.co.za>
2023-08-09 12:41:04 -0700

    the zen

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,24 +1,3 @@
 #!/usr/bin/python3
-i = " is better than i"
-t = "If the implementation is "
-s = " to explain, it"
-print(f"The Zen of Python, by Tim Peters\n\n"\
-        f"Beautiful{i}ugly.\n"\
-        f"Explicit{i}implicit.\n"\
-        f"Simple{i}complex.\n"\
-        f"Complex{i}complicated.\n"\
-        f"Flat{i}nested.\n"\
-        f"Sparse{i}dense.\n"\
-        f"Readability counts.\n"\
-        f"Special cases aren't special enough to break the rules.\n"\
-        f"Although practicality beats purity.\n"\
-        f"Errors should never pass silently.\n"\
-        f"Unless explicitly silenced.\n"\
-        f"In the face of ambiguity, refuse the temptation to guess.\n"\
-        f"There should be one-- and preferably only one --obvious way to do it.\n"\
-        f"Although that way may not be obvious at first unless you're Dutch.\n"\
-        f"Now is better than never.\n"\
-        f"Although never is often better than *right* now.\n"\
-        f"{t}hard{s}'s a bad idea.\n"\
-        f"{t}easy{s} may be a good idea.\n"\
-        f"Namespaces are one honking great idea -- let's do more of those!")
+f = open("./the-zen", "r")
+print(f.read())
