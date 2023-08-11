86056dd4934d365bdd5f508ac1dbf9d4809ae641
skd-alt <sipho@skanalytics.co.za>
2023-08-11 13:09:39 -0700

    Sys argv tutorial

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,12 @@
 if __name__ == "__main__":
     from sys import argv
 
-    args = len(argv) - 1
-    if args == 0:
+    count = len(argv) - 1
+    if count == 0:
         print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(args))
-        for i in range (args):
-            print("{}: {}".format(i + 1, argv[i + 1]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, argv[i + 1]))
