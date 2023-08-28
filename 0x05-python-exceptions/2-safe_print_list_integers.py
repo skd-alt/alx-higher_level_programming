0affeda2ae8c41eb78863a42d5e9958e8a880093
skd-alt <sipho@skanalytics.co.za>
2023-08-28 12:03:32 -0700

    Print int only in a list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            continue
+        except (IndexError):
+            break
+        else:
+            num += 1
+    print("")
+    return num
