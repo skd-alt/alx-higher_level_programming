c6d011cce71de1cc95b90da6b93cdc595d78e39d
skd-alt <sipho@skanalytics.co.za>
2023-08-28 13:22:39 -0700

    handle all errors

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         result = fct(*args)
-    except:
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
     else:
