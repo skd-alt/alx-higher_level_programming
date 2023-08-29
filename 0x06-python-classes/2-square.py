44cd00cb604156c61e1d9c6baffbccf3ead4c609
skd-alt <sipho@skanalytics.co.za>
2023-08-29 14:27:05 -0700

    Square with raised errors

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A Square Class"""
+
+
+class Square:
+    """Square class with Private instance attribute"""
+
+    def __init__(self, size=0):
+        """ Intializes a Square class object
+
+        Note:
+            size is a private attribute
+
+        Args:
+            size: Lenghth of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
