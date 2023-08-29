2d2a7292e2d1715001f348aa1e4720be84cf5a7f
skd-alt <sipho@skanalytics.co.za>
2023-08-29 14:47:08 -0700

    Add Getter and Setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A Square Class"""
+
+
+class Square:
+    """Square class with Private instance attribute"""
+
+    def __init__(self, size=0):
+        """ Intializes a Square class object
+
+        Note:
+            size is a private attribute
+
+        Args:
+            size: Lenghth of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, new_size):
+        if not isinstance(new_size, int):
+            raise TypeError("size must be an integer")
+        elif new_size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = new_size
+
+    def area(self):
+        """Calculate area of square"""
+        return self.__size ** 2
