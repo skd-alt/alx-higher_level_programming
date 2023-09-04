91a4dd3cd713893a907e9540235439d05660b14a
skd-alt <sipho@skanalytics.co.za>
2023-09-04 12:23:39 -0700

    Rectangle Class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Create a Rectangle class"""
 
+"""Define a Rectangle class."""
 
 class Rectangle:
-    """Repreesents a Rectangle """
+    """Represent a rectangle."""
     pass
