7eaf8fc0b01ea0640f60b49c26889f53c2fcde23
skd-alt <sipho@skanalytics.co.za>
2023-09-11 11:33:17 -0700

    list obj attributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Creates look up object attribute function"""
 
+
 def lookup(obj):
     """function returning a list attributes and methods of an object"""
     return dir(obj)
