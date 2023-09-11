24b633bcf9a123b1016f22fe3bba41ee4259a9ad
skd-alt <sipho@skanalytics.co.za>
2023-09-11 16:02:33 -0700

    add attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that adds an attribute to a class"""
+
+def add_attribute(a_class, attr, value):
+    """Sets new attribute in class"""
+    if "__dict__" in dir(a_class):
+        a_class.__setattr__(attr, value)
+    else:
+        raise TypeError("can't add new attribute")
