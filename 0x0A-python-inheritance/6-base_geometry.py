2a70530b7866db972babf2196348afd95a630df4
skd-alt <sipho@skanalytics.co.za>
2023-09-11 13:11:56 -0700

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""An class BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class"""
+    
+    def area(self):
+        """Raise Exception when run"""
+        raise Exception("area() is not implemented")
