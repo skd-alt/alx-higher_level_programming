b6657b415170dc6fac51511b3b8defc6cc35f4bf
skd-alt <sipho@skanalytics.co.za>
2023-09-12 15:23:13 -0700

    Replace attr from json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Creates a Student Class"""
+
+
+class Student:
+    """Student object is created"""
+    def __init__(self, first_name, last_name, age):
+        """Intializes Student attrs"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return json represention in dictionary"""
+        if attrs is None:
+            return self.__dict__
+        d = {a: self.__getattribute__(a) for a in attrs if a in self.__dict__}
+        return d
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student"""
+        for attr in json:
+            if attr in self.__dict__:
+                self.__dict__[attr] = json[attr]
