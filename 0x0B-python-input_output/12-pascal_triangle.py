332e99759b022977f6496e2123baac4164594106
skd-alt <sipho@skanalytics.co.za>
2023-09-12 16:18:38 -0700

    Pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a Pascal's Triangle function."""
+
+
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
+    """
+    d = []
+    x = 5
+    for i in range(x):
+        if i == 0:
+            d.append([1])
+        else:
+            temp = []
+            for j in range(i + 1):
+
+                if j == 0:
+                    temp.append(1)
+                elif j < i:
+                    temp.append(d[i - 1][j - 1] + d[i - 1][j])
+                else:
+                    temp.append(d[i - 1][j - 1])
+            d.append(temp)
+    return(d)
