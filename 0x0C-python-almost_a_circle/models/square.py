d6908cc59d9b02fd402e91088fceb331e7af750a
skd-alt <sipho@skanalytics.co.za>
2023-09-18 11:23:58 -0700

    Create Square Clas

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Class Square inherits from Rectangle and base
+"""
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """Square class inheriting from rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """intializes rectangle as a sqaure"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return Square attributes"""
+        txt = f"[Square] ({self.id}) {self.x}/{self.y} "
+        txt += f"- {self.width}"
+        return txt
