066b27a81bd1a473b4938f992f766945f71e1229
skd-alt <sipho@skanalytics.co.za>
2023-09-18 11:50:51 -0700

    Sqaure update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,6 +15,21 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return Square attributes"""
-        txt = f"[Square] ({self.id}) {self.x}/{self.y} "
-        txt += f"- {self.width}"
-        return txt
+        txt2 = f"[Square] ({self.id}) {self.x}/{self.y} "
+        txt2 += f"- {self.width}"
+        return txt2
+
+    @property
+    def size(self):
+        """Gets the size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of sqaure"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
