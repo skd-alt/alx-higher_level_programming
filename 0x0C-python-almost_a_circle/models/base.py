fb6c25f1492fd1785fcac989ba5e7712d2f5a751
skd-alt <sipho@skanalytics.co.za>
2023-09-18 13:48:08 -0700

    Create a new instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,3 +49,13 @@ class Base:
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Creates an instance of a class. """
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 2)
+        else:
+            new = cls(1)
+        new.update(**dictionary)
+        return new
