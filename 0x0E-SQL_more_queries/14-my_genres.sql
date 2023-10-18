e6df8118d076713ed0555d206ed70a70a6220a5f
skd-alt <sipho@skanalytics.co.za>
2023-10-18 15:31:29 -0700

    Multiple joins

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- lists all genres of the show Dexter
+SELECT tv_genres.name AS name
+FROM tv_show_genres
+	JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY name;
