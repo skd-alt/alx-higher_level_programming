ea51ed26aaf9c8a75fafb73c545a0d46603848c0
skd-alt <sipho@skanalytics.co.za>
2023-10-18 13:04:23 -0700

    Create User with privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Create User with password and grant permmissions
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
