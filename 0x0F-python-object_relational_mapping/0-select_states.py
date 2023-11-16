585553396278fe80495a2635adf1778294c96174
skd-alt <sipho@skanalytics.co.za>
2023-11-16 20:10:27 +0000

    Get states from database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    conn = MySQLdb.connect(
+            host="localhost", port=3360, user=argv[1],
+            passwd=argv[2], db=argv[3], charset="utf8"
+            )
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
