ea4931ed6b3baa13eaf8ac2a73ef56691daf9c30
skd-alt <sipho@skanalytics.co.za>
2023-11-19 17:38:25 +0000

    Fetch all states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,9 +8,12 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-            format(argv[1], argv[2], argv[3]), 
-            pool_pre_ping=True)
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.
+            format(argv[1], argv[2], argv[3]),
+            pool_pre_ping=True
+            )
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
