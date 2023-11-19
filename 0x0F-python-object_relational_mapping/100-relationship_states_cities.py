894beabb4d6260697c6826234cee6e420aa76b34
skd-alt <sipho@skanalytics.co.za>
2023-11-19 21:56:43 +0000

    Relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""creates the State California with the City San Francisco"""
-
-from sys import argv
+"""
+Creates the State "California" with the City "San Francisco" from a DB
+"""
+import sys
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import create_engine
@@ -9,19 +10,18 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.
-            format(argv[1], argv[2], argv[3]),
-            pool_pre_ping=True
-            )
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name='California')
-    new_city = City(name='San Francisco')
-    new_state.cities.append(new_city)
+    newState = State(name='California')
+    newCity = City(name='San Francisco')
+    newState.cities.append(newCity)
 
-    session.add(new_state)
-    session.add(new_city)
+    session.add(newState)
+    session.add(newCity)
     session.commit()
