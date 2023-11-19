227d75e5a764d2671fd50f86fecbbbc3f4be2d83
skd-alt <sipho@skanalytics.co.za>
2023-11-19 20:12:34 +0000

    Add State

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Print first State object from the database hbtn_0e_6_usa"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine, bindparam, select
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.
+            format(argv[1], argv[2], argv[3]),
+            pool_pre_ping=True
+            )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
