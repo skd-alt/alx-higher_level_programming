ef4c80cee254775d5993860cdee2fa0f3945f408
skd-alt <sipho@skanalytics.co.za>
2023-11-19 17:06:19 +0000

    State class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+defines the States Class model
+"""
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """
+    State class defined
+    id: primary key
+    name: the state name
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
