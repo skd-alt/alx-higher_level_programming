4c4f9ddf0a8b6f30c014302fc179fa45d0d36968
skd-alt <sipho@skanalytics.co.za>
2023-12-01 10:56:13 -0800

    Requests handle errors

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""manage urllib.error.HTTPError exceptions and print: Error code:"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
