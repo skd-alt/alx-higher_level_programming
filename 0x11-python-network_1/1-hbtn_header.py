90223ecc9f767f34118f157993ce9fce36886f6c
skd-alt <sipho@skanalytics.co.za>
2023-12-01 17:54:20 +0000

    Response header value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Takes in URL requests it and gets the X-Request id"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        print(dict(response.headers).get("X-Request-Id"))
