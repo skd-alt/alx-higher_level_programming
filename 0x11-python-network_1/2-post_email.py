c5b76ab0a9af5e1f617b1dc11ddf272ac464e4bb
skd-alt <sipho@skanalytics.co.za>
2023-12-01 10:13:06 -0800

    POST an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email as a parameter"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
